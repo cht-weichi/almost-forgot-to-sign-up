3cc279b75b84d299cca66d22e6a9cb8ef4475204
weichi wu <weichiwu@cht.com.tw>
2022-04-28 15:59:58 +0800

    fix: logo & iphone text

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,6 +1,101 @@
 (function($) {
   'use strict';
   $(function() {
+    if ($("#PeopleCounts").length) {
+      var graphGradient = document.getElementById("PeopleCounts").getContext('2d');
+      var graphGradient2 = document.getElementById("PeopleCounts").getContext('2d');
+      var saleGradientBg = graphGradient.createLinearGradient(5, 0, 5, 100);
+      saleGradientBg.addColorStop(0, 'rgba(7, 211, 130, 0.87)');
+      saleGradientBg.addColorStop(1, 'rgba(7, 211, 130, 0.02)');
+      var salesTopData = {
+          labels: Array.from({length: 30}, (_, i) => i + 1),
+          datasets: [{
+            label: '服務時間長度',
+            data: [
+              176, 185, 289, 203, 381, 389, 92,
+              184, 227, 214, 218, 393, 352, 126,
+              171, 202, 284, 323, 339, 379, 131,
+              153, 169, 276, 353, 431, 421, 107,
+              142, 157
+            ],
+            backgroundColor: saleGradientBg,
+            borderColor: '#47D382',
+            borderWidth: 1.5,
+            fill: true, // 3: no fill
+            pointBorderWidth: 1,
+            pointRadius: 4,
+            pointHoverRadius: 10,
+            pointHitRadius: 15,
+            pointBackgroundColor: '#47D382',
+            pointBorderColor: '#fff',
+        }]
+      };
+  
+      var salesTopOptions = {
+        responsive: true,
+        maintainAspectRatio: false,
+          scales: {
+              yAxes: [{
+                  gridLines: {
+                      display: true,
+                      drawBorder: false,
+                      color:"#F0F0F0",
+                      zeroLineColor: '#F0F0F0',
+                  },
+                  ticks: {
+                    beginAtZero: false,
+                    autoSkip: true,
+                    maxTicksLimit: 4,
+                    fontSize: 12,
+                    color:"#6B778C"
+                  }
+              }],
+              xAxes: [{
+                gridLines: {
+                    display: false,
+                    drawBorder: false,
+                },
+                ticks: {
+                  beginAtZero: false,
+                  autoSkip: true,
+                  // maxTicksLimit: 7,
+                  fontSize: 12,
+                  color:"#6B778C"
+                }
+            }],
+          },
+          legend:false,
+          legendCallback: function (chart) {
+            var text = [];
+            text.push('<div class="chartjs-legend"><ul>');
+            // for (var i = 0; i < chart.data.datasets.length; i++) {
+            //   console.log(chart.data.datasets[i]); // see what's inside the obj.
+            //   text.push('<li>');
+            //   text.push('<span style="background-color:' + chart.data.datasets[i].borderColor + '">' + '</span>');
+            //   text.push(chart.data.datasets[i].label);
+            //   text.push('</li>');
+            // }
+            text.push('<li class="align-items-baseline"><h5 class="card-subtitle card-subtitle-dash">平均</h5><h2 class="card-subtitle card-subtitle-dash fw-bold" style="color: #47D382;">19.6</h2><h5 class="card-subtitle card-subtitle-dash">分鐘</h5></li>');
+            text.push('</ul></div>');
+            return text.join("");
+          },
+          
+          elements: {
+              line: {
+                  tension: 0.4,
+              }
+          },
+          tooltips: {
+              backgroundColor: 'rgba(31, 59, 179, 1)',
+          }
+      }
+      var salesTop = new Chart(graphGradient, {
+          type: 'line',
+          data: salesTopData,
+          options: salesTopOptions
+      });
+      document.getElementById('PeopleCounts-legend').innerHTML = salesTop.generateLegend();
+    }
     if ($("#ServiceTime").length) {
       var graphGradient = document.getElementById("ServiceTime").getContext('2d');
       var graphGradient2 = document.getElementById("ServiceTime").getContext('2d');
